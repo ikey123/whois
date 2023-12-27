6de99176b4ae9c877b2d74770dda89b1f5a7e922
ikey123 <144579334+ikey123@users.noreply.github.com>
2023-12-27 19:05:22 +0800

    Update whois.js

--- api/whois.js
+++ api/whois.js
@@ -1,24 +1,24 @@
-// Import necessary modules
-const fetch = require('node-fetch');
-
-// Handler for the serverless function
 module.exports = async (req, res) => {
-  const { name, suffix } = req.query; // Get query parameters from the request URL
+    try {
+        const { name, suffix } = req.query;
+        const apiUrl = `https://whois.freeaiapi.xyz/?name=${encodeURIComponent(name)}&suffix=${encodeURIComponent(suffix)}`;
+        
+        // 输出请求信息到日志中
+        console.log('Request URL:', apiUrl);
 
-  // Make the request to the external API
-  try {
-    const apiUrl = `https://whois.freeaiapi.xyz/?name=${encodeURIComponent(name)}&suffix=${encodeURIComponent(suffix)}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-    // Set the appropriate headers to allow cross-origin requests
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        // 输出响应数据到日志中
+        console.log('Response data:', data);
 
-    // Return the response from the external API
-    res.status(response.status).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+        res.status(response.status).json(data);
+    } catch (error) {
+        console.error('Error occurred in serverless function:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
