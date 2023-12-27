ab0a23bfadc60bcaef53d5117cea500f9103fcd4
ikey123 <144579334+ikey123@users.noreply.github.com>
2023-12-27 20:58:07 +0800

    Update whois.js

--- api/whois.js
+++ api/whois.js
@@ -1,5 +1,3 @@
-// api/whois.js
-
 module.exports = async (req, res) => {
   try {
     const { name, suffix } = req.query;
@@ -10,6 +8,8 @@ module.exports = async (req, res) => {
 
     const apiUrl = `https://whois.freeaiapi.xyz/?name=${encodeURIComponent(name)}&suffix=${encodeURIComponent(suffix)}`;
     
+    console.log('Request URL:', apiUrl);
+
     const response = await fetch(apiUrl);
     
     if (!response.ok) {
@@ -18,12 +18,15 @@ module.exports = async (req, res) => {
 
     const responseData = await response.json();
 
+    console.log('Response Data:', responseData);
+
     if (responseData.success) {
       res.status(200).json({ whoisData: responseData.whoisData });
     } else {
       res.status(500).json({ error: 'Failed to fetch Whois information.' });
     }
   } catch (error) {
+    console.error('An error occurred:', error);
     res.status(500).json({ error: error.message || 'Internal server error' });
   }
 };
